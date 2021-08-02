708de66ad68492dfb670c877063417d2f1400bdd
Shinnosuke Sawada <6warashi9@gmail.com>
2021-08-02 16:45:44 +0900

    update newpost.sh

--- newpost.sh
+++ newpost.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-set -eo pipefail
+#!/bin/bash
+set -euo pipefail
 title="${1}"
 if [ -z "${title}" ]; then
 	echo "Usage: ${0} 'title'"
@@ -8,7 +8,6 @@ fi
 
 path="post/$(date -j '+%Y/%m/%d')/${title}.md"
 git fetch origin -p
-git checkout origin/source
-git checkout -b feature/${title}
-hugo new ${path}
-open content/${path}
+git switch -c "feature/${title}" origin/main
+hugo new "${path}"
+open "content/${path}"
