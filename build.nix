b4a19e79be33478aaf80b7aade2ed7165187555c
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2025-03-07 21:18:24 +0900

    style: nixfmt

--- build.nix
+++ build.nix
@@ -4,56 +4,64 @@
   theme,
   baseURL,
   ...
-}: let
+}:
+let
   callPackage = lib.callPackageWith (pkgs // packages);
   packages = {
-    contents = callPackage ({
-      runCommand,
-      emacsPackagesFor,
-      emacs,
-    }:
-      runCommand "contents" {
-        src = ./contents.org;
-        nativeBuildInputs = [
-          ((emacsPackagesFor emacs).emacsWithPackages (epkgs:
-            with epkgs; [
-              org
-              ox-hugo
-            ]))
-        ];
-      } ''
-        mkdir -p $out/static
-        emacs --batch --eval "(progn (require 'org) (require 'ox-hugo) (setq org-hugo-base-dir \"$out\") (with-current-buffer (find-file-noselect \"$src\") (org-hugo-export-wim-to-md t)))"
-      '') {};
-    themes = callPackage ({
-      runCommand,
-      theme,
-    }:
-      runCommand "themes" {
-        src = theme;
-      } ''
+    contents = callPackage (
+      {
+        runCommand,
+        emacsPackagesFor,
+        emacs,
+      }:
+      runCommand "contents"
+        {
+          src = ./contents.org;
+          nativeBuildInputs = [
+            ((emacsPackagesFor emacs).emacsWithPackages (
+              epkgs: with epkgs; [
+                org
+                ox-hugo
+              ]
+            ))
+          ];
+        }
+        ''
+          mkdir -p $out/static
+          emacs --batch --eval "(progn (require 'org) (require 'ox-hugo) (setq org-hugo-base-dir \"$out\") (with-current-buffer (find-file-noselect \"$src\") (org-hugo-export-wim-to-md t)))"
+        ''
+    ) { };
+    themes = callPackage (
+      { runCommand, theme }:
+      runCommand "themes" { src = theme; } ''
         mkdir -p $out
         cp -r $src $out/theme
-      '') {inherit theme;};
-    public = callPackage ({
-      runCommand,
-      go,
-      hugo,
-      nodejs,
-      contents,
-      themes,
-    }:
-      runCommand "public" {
-        nativeBuildInputs = [
-          go
-          hugo
-          nodejs
-        ];
-      } ''
-        hugo --minify --noBuildLock --environment production --baseURL ${baseURL} --source ${./.} --theme theme --themesDir ${themes.outPath} --contentDir ${contents.outPath}/content --destination $out
-      '') {};
+      ''
+    ) { inherit theme; };
+    public = callPackage (
+      {
+        runCommand,
+        go,
+        hugo,
+        nodejs,
+        contents,
+        themes,
+      }:
+      runCommand "public"
+        {
+          nativeBuildInputs = [
+            go
+            hugo
+            nodejs
+          ];
+        }
+        ''
+          hugo --minify --noBuildLock --environment production --baseURL ${baseURL} --source ${./.} --theme theme --themesDir ${themes.outPath} --contentDir ${contents.outPath}/content --destination $out
+        ''
+    ) { };
   };
-in {
+in
+{
   inherit packages;
   default = packages.public;
 }
